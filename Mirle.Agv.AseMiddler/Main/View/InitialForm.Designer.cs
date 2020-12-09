732dc5dc86468ba9b7cfcdbfd6e0bb1e9f9152a2
yiminglien@mirle.com.tw <yiminglien@mirle.com.tw>
2020-12-09 14:13:33 +0800

    Update Mirle_Logo into 2020 version 20.12.09.01

--- Mirle.Agv.AseMiddler/Main/View/InitialForm.Designer.cs
+++ Mirle.Agv.AseMiddler/Main/View/InitialForm.Designer.cs
@@ -34,13 +34,9 @@
             this.Label1 = new System.Windows.Forms.Label();
             this.Label4 = new System.Windows.Forms.Label();
             this.lst_StartUpMsg = new System.Windows.Forms.ListBox();
-            this.PictureBox3 = new System.Windows.Forms.PictureBox();
-            this.PictureBox2 = new System.Windows.Forms.PictureBox();
             this.cmd_Close = new System.Windows.Forms.Button();
-            this.PictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Label3
@@ -78,9 +74,9 @@
             this.Label4.BackColor = System.Drawing.Color.LightGreen;
             this.Label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Label4.Location = new System.Drawing.Point(419, 1);
+            this.Label4.Location = new System.Drawing.Point(404, 1);
             this.Label4.Name = "Label4";
-            this.Label4.Size = new System.Drawing.Size(526, 80);
+            this.Label4.Size = new System.Drawing.Size(541, 80);
             this.Label4.TabIndex = 21;
             this.Label4.Text = "盟立 AGV";
             this.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -90,35 +86,16 @@
             this.lst_StartUpMsg.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lst_StartUpMsg.FormattingEnabled = true;
             this.lst_StartUpMsg.ItemHeight = 16;
-            this.lst_StartUpMsg.Location = new System.Drawing.Point(419, 84);
+            this.lst_StartUpMsg.Location = new System.Drawing.Point(408, 85);
             this.lst_StartUpMsg.Name = "lst_StartUpMsg";
-            this.lst_StartUpMsg.Size = new System.Drawing.Size(526, 276);
+            this.lst_StartUpMsg.Size = new System.Drawing.Size(538, 276);
             this.lst_StartUpMsg.TabIndex = 22;
-            // 
-            // PictureBox3
-            // 
-            this.PictureBox3.Image = global::Mirle.Agv.AseMiddler.Properties.Resources.Mirle_Slogan;
-            this.PictureBox3.Location = new System.Drawing.Point(165, 12);
-            this.PictureBox3.Name = "PictureBox3";
-            this.PictureBox3.Size = new System.Drawing.Size(219, 31);
-            this.PictureBox3.TabIndex = 24;
-            this.PictureBox3.TabStop = false;
-            // 
-            // PictureBox2
-            // 
-            this.PictureBox2.Image = global::Mirle.Agv.AseMiddler.Properties.Resources.Mirle_LOGO;
-            this.PictureBox2.Location = new System.Drawing.Point(-4, 1);
-            this.PictureBox2.Name = "PictureBox2";
-            this.PictureBox2.Size = new System.Drawing.Size(141, 48);
-            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.PictureBox2.TabIndex = 23;
-            this.PictureBox2.TabStop = false;
             // 
             // cmd_Close
             // 
             this.cmd_Close.Image = ((System.Drawing.Image)(resources.GetObject("cmd_Close.Image")));
             this.cmd_Close.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.cmd_Close.Location = new System.Drawing.Point(330, 294);
+            this.cmd_Close.Location = new System.Drawing.Point(319, 284);
             this.cmd_Close.Name = "cmd_Close";
             this.cmd_Close.Size = new System.Drawing.Size(83, 58);
             this.cmd_Close.TabIndex = 20;
@@ -127,21 +104,21 @@
             this.cmd_Close.UseVisualStyleBackColor = true;
             this.cmd_Close.Click += new System.EventHandler(this.cmd_Close_Click);
             // 
-            // PictureBox1
+            // PictureBox2
             // 
-            this.PictureBox1.Image = global::Mirle.Agv.AseMiddler.Properties.Resources.Mirle_Home;
-            this.PictureBox1.Location = new System.Drawing.Point(-4, 1);
-            this.PictureBox1.Name = "PictureBox1";
-            this.PictureBox1.Size = new System.Drawing.Size(421, 268);
-            this.PictureBox1.TabIndex = 12;
-            this.PictureBox1.TabStop = false;
+            this.PictureBox2.Image = global::Mirle.Agv.AseMiddler.Properties.Resources.Mirle_Logo22;
+            this.PictureBox2.Location = new System.Drawing.Point(-4, 1);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(412, 268);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 23;
+            this.PictureBox2.TabStop = false;
             // 
             // InitialForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(947, 358);
-            this.Controls.Add(this.PictureBox3);
             this.Controls.Add(this.PictureBox2);
             this.Controls.Add(this.lst_StartUpMsg);
             this.Controls.Add(this.Label4);
@@ -149,23 +126,18 @@
             this.Controls.Add(this.Label3);
             this.Controls.Add(this.Label2);
             this.Controls.Add(this.Label1);
-            this.Controls.Add(this.PictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "InitialForm";
             this.Text = "InitialForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InitialForm_FormClosing);
             this.Shown += new System.EventHandler(this.InitialForm_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        internal System.Windows.Forms.PictureBox PictureBox1;
         internal System.Windows.Forms.Label Label3;
         internal System.Windows.Forms.Label Label2;
         internal System.Windows.Forms.Label Label1;
@@ -173,6 +145,5 @@
         internal System.Windows.Forms.Label Label4;
         internal System.Windows.Forms.ListBox lst_StartUpMsg;
         internal System.Windows.Forms.PictureBox PictureBox2;
-        internal System.Windows.Forms.PictureBox PictureBox3;
     }
 }
