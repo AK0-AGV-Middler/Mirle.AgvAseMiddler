022afaf5b7bdb079a75e24e8e50e26241cb7863d
yiminglien@mirle.com.tw <yiminglien@mirle.com.tw>
2020-12-09 14:13:33 +0800

    Update Mirle_Logo into 2020 version

--- Mirle.Agv.AseMiddler/Properties/Resources.Designer.cs
+++ Mirle.Agv.AseMiddler/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Mirle.Agv.AseMiddler.Properties {
     // 類別透過 ResGen 或 Visual Studio 這類工具。
     // 若要加入或移除成員，請編輯您的 .ResX 檔，然後重新執行 ResGen
     // (利用 /str 選項)，或重建您的 VS 專案。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -146,6 +146,36 @@ namespace Mirle.Agv.AseMiddler.Properties {
         internal static System.Drawing.Bitmap Mirle_LOGO {
             get {
                 object obj = ResourceManager.GetObject("Mirle_LOGO", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap Mirle_Logo2 {
+            get {
+                object obj = ResourceManager.GetObject("Mirle_Logo2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap Mirle_Logo21 {
+            get {
+                object obj = ResourceManager.GetObject("Mirle_Logo21", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap Mirle_Logo22 {
+            get {
+                object obj = ResourceManager.GetObject("Mirle_Logo22", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
